f48a78381f1d89d1466c5b366d62bd267d3f7d41
exploiitm <exploiim24@gmail.com>
2025-11-25 20:33:11 +0530

    Chore: gpio cleanup

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,7 +1,7 @@
 use core::marker::PhantomData;
 use paste::paste;
 
-use embedded_hal::digital::{ErrorType, InputPin, OutputPin};
+use embedded_hal::digital::{ErrorType, OutputPin};
 
 pub trait PinMode {}
 
@@ -25,11 +25,11 @@ macro_rules! gpio {
     ($GPIO_PORT:ident, $GPIO_NUM:literal, $GPIO_PAC: ident, [$($PIN_NUMS:literal),*]) => {
 
         paste! {
-            pub mod [<g p i o $GPIO_PORT _ m o d>] {
+            pub mod [<gpio $GPIO_PORT _ m o d>] {
 
                 use crate::pac::$GPIO_PAC as $GPIO_PAC;
-                use crate::pac::Iomux as Iomux; 
-                use super::{InputPin, OutputPin, ErrorType, Input, Output, PinMode};
+                use crate::pac::Iomux as Iomux;
+                use super::{OutputPin, ErrorType, Input, Output, PinMode};
                 use super::PhantomData;
 
 
@@ -44,12 +44,16 @@ macro_rules! gpio {
 
                     fn enable_output(&self, iomux: Iomux) {
                         let gpio = unsafe { &*$GPIO_PAC::ptr() };
-                        iomux.iomux_pincm(41).write(|w| unsafe { w.bits(0x80 | 0x1) });
-                        gpio.[<g p i o $GPIO_PORT _ d o e s e t 3 1 _ 0>]().write(|w| unsafe { w.bits(1 << N) });
+                        iomux.iomux_pincm(41).write(|w| { 
+                            unsafe { w.pf().bits(0x1) };
+                            w.pc().connected()
+                        });
+                        gpio.[<gpio $GPIO_PORT _ doeset31_0>]().write(|w| 
+                            unsafe { w.bits(1 << N) });
                     }
 
                     pub fn into_output(&self, iomux: Iomux) -> Pin::<N, Output> {
-                        let mut pin = Pin::<N, Output>::new();
+                        let pin = Pin::<N, Output>::new();
                         pin.enable_output(iomux);
                         pin
                     }
@@ -58,11 +62,13 @@ macro_rules! gpio {
                 impl<const N: u8> Pin<N, Output> {
                     pub fn set_low(&mut self) {
                         let gpio = unsafe { &*$GPIO_PAC::ptr() };
-                        gpio.[<g p i o $GPIO_PORT _ d o u t c l r 3 1 _ 0>]().write(|w| unsafe { w.bits(1 << N) });
+                        gpio.[<gpio $GPIO_PORT _ doutclr31_0>]().write(|w|
+                            unsafe { w.bits(1 << N) });
                     }
                     pub fn set_high(&mut self) {
                         let gpio = unsafe { &*$GPIO_PAC::ptr() };
-                        gpio.[<g p i o $GPIO_PORT _ d o u t s e t 3 1 _ 0>]().write(|w| unsafe { w.bits(1 << N) });
+                        gpio.[<gpio $GPIO_PORT _ doutset31_0>]().write(|w|
+                            unsafe { w.bits(1 << N) });
                     }
                 }
 
@@ -73,18 +79,20 @@ macro_rules! gpio {
                 impl<const N: u8> OutputPin for Pin<N, Output> {
                     fn set_low(&mut self) -> Result<(), Self::Error> {
                         let gpio = unsafe { &*$GPIO_PAC::ptr() };
-                        gpio.[<g p i o $GPIO_PORT _ d o u t c l r 3 1 _ 0>]().write(|w| unsafe { w.bits(1 << N) });
+                        gpio.[<gpio $GPIO_PORT _ doutclr31_0>]().write(|w|
+                            unsafe { w.bits(1 << N) });
                         Ok(())
                     }
                     fn set_high(&mut self)-> Result<(), Self::Error>  {
                         let gpio = unsafe { &*$GPIO_PAC::ptr() };
-                        gpio.[<g p i o $GPIO_PORT _ d o u t s e t 3 1 _ 0>]().write(|w| unsafe { w.bits(1 << N) });
+                        gpio.[<gpio $GPIO_PORT _ doutset31_0>]().write(|w|
+                            unsafe { w.bits(1 << N) });
                         Ok(())
                     }
                 }
 
                 pub struct Pins {
-                    $(pub [<p i n $PIN_NUMS>]: Pin<$PIN_NUMS>),+
+                    $(pub [<pin $PIN_NUMS>]: Pin<$PIN_NUMS>),+
                 }
 
                 pub struct GpioPeripheral {
@@ -93,16 +101,15 @@ macro_rules! gpio {
 
                 impl GpioPeripheral {
                     pub fn new(gpio: $GPIO_PAC) -> Self {
-                        const RSTCTL_KEY_UNLOCK: u32 = 0xB100_0000;
-                        const RSTCTL_STICKY_CLEAR: u32 = 0x0000_0002;
-                        const RSTCTL_ASSERT: u32 = 0x0000_0001;
-
-                        gpio.[<g p i o $GPIO_PORT _ g p r c m>](0).[<g p i o $GPIO_PORT _ r s t c t l>]().write(|w| unsafe { w.bits(RSTCTL_KEY_UNLOCK | RSTCTL_STICKY_CLEAR | RSTCTL_ASSERT) });
-
-                        const PWREN_KEY_UNLOCK: u32 = 0x2600_0000;
-                        const PWREN_ENABLE: u32 = 0x0000_0001;
-
-                        gpio.[<g p i o $GPIO_PORT _ g p r c m>](0).[<g p i o $GPIO_PORT _ p w r e n>]().write(|w| unsafe { w.bits(PWREN_KEY_UNLOCK | PWREN_ENABLE) });
+                        gpio.[<gpio $GPIO_PORT _ gprcm>](0).[<gpio $GPIO_PORT _ rstctl>]().write(|w| {
+                            w.resetassert().assert();
+                            w.resetstkyclr().clr();
+                            w.key_unlock().unlock()
+                        });
+                        gpio.[<gpio $GPIO_PORT _ gprcm>](0).[<gpio $GPIO_PORT _ pwren>]().write(|w| {
+                            w.enable().enable();
+                            w.key_unlock().unlock()
+                        });
 
                         Self {
                             _gpio: gpio
@@ -112,13 +119,13 @@ macro_rules! gpio {
                     pub fn pins(&self) -> Pins {
 
                         Pins {
-                            $([<p i n $PIN_NUMS>]: Pin::<$PIN_NUMS>::new()),+
+                            $([<pin $PIN_NUMS>]: Pin::<$PIN_NUMS>::new()),+
                         }
 
                     }
                 }
             }
-            pub use [<g p i o $GPIO_PORT _ m o d>]::GpioPeripheral as [<g p i o $GPIO_PORT>];
+            pub use [<gpio $GPIO_PORT _ mod>]::GpioPeripheral as [<gpio $GPIO_PORT>];
         }
     }
 }
@@ -128,7 +135,8 @@ gpio!(
     0,
     Gpioa,
     [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+        20, 21
     ]
 );
 // gpio!(b, 1, Gpiob, [0, 1, 2, 3]);
