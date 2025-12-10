f9b2990d5ab160a309cbb641e2eadd43ad9b8c53
exploiitm <exploiim24@gmail.com>
2025-12-10 21:24:18 +0530

    Random stuff

--- src/aes/mod.rs
+++ src/aes/mod.rs
@@ -39,7 +39,41 @@ impl AesAdv {
 
         self._aes
             .aesadv_data0()
+            .write(|w| unsafe { w.bits(0x11111111) });
+        self._aes
+            .aesadv_data1()
             .write(|w| unsafe { w.bits(0x00000000) });
+        self._aes
+            .aesadv_data2()
+            .write(|w| unsafe { w.bits(0x00000000) });
+        self._aes
+            .aesadv_data3()
+            .write(|w| unsafe { w.bits(0x00000000) });
+
+        while self._aes.aesadv_ctrl().read().output_rdy().is_notready() {}
+
+        [
+            self._aes.aesadv_data0().read().bits(),
+            self._aes.aesadv_data1().read().bits(),
+            self._aes.aesadv_data2().read().bits(),
+            self._aes.aesadv_data3().read().bits(),
+        ]
+    }
+    pub fn test2(&self) -> [u32; 4] {
+        while self._aes.aesadv_ctrl().read().cntxt_rdy().is_notready() {}
+
+        // Key should be stored in register -_- imagine...
+
+
+        self._aes.aesadv_ctrl().write(|w| {
+            w.save_cntxt().no_effect();
+            w.keysize().k128();
+            w.dir().encrypt()
+        });
+
+        self._aes
+            .aesadv_data0()
+            .write(|w| unsafe { w.bits(0x2) });
         self._aes
             .aesadv_data1()
             .write(|w| unsafe { w.bits(0x00000000) });
