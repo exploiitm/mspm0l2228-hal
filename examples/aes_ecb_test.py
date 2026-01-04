cc110235ac02b67ed77c7bf4c61c76b7a68fb956
rishit khandelwal <rishitkh@gmail.com>
2026-01-04 20:40:25 +0530

    Fix ECB test script

--- examples/aes_ecb_test.py
+++ examples/aes_ecb_test.py
@@ -3,7 +3,10 @@ import time
 import subprocess
 from Crypto.Cipher import AES
 
-ser = serial.Serial('/dev/ttyACM0', timeout=10)
+ser = serial.Serial('/dev/ttyACM0', timeout=1)
+while ser.read(1):
+    pass
+
 subprocess.Popen(["bash", "reset.sh"], stderr=subprocess.DEVNULL)
 start_frame = bytes.fromhex("deadc0de")
 buffer = b"\x00"*4
@@ -19,32 +22,28 @@ key = ser.read(16)
 print("[+] Recieved KEY")
 
 for i in range(5):
-    iv = ser.read(16)
-    cipher = AES.new(key, AES.MODE_ECB, iv)
+    cipher = AES.new(key, AES.MODE_ECB)
     pt = ser.read(16)
     ct = ser.read(16)
     assert cipher.encrypt(pt) == ct, f"[!] Passed {i}, Failed {5-i}"
 print("[+] Passed ECB Enc Tests [NoDma]")
 
 for i in range(5):
-    iv = ser.read(16)
-    cipher = AES.new(key, AES.MODE_ECB, iv)
+    cipher = AES.new(key, AES.MODE_ECB)
     pt = ser.read(16)
     ct = ser.read(16)
     assert cipher.decrypt(pt) == ct, f"[!] Passed {i}, Failed {5-i}"
 print("[+] Passed ECB Dec Tests [NoDma]")
 
 for i in range(5):
-    iv = ser.read(16)
-    cipher = AES.new(key, AES.MODE_ECB, iv)
+    cipher = AES.new(key, AES.MODE_ECB)
     pt = ser.read(64)
     ct = ser.read(64)
     assert cipher.encrypt(pt) == ct, f"[!] Passed {i}, Failed {5-i}"
 print("[+] Passed ECB Enc Tests [WithDma]")
 
 for i in range(5):
-    iv = ser.read(16)
-    cipher = AES.new(key, AES.MODE_ECB, iv)
+    cipher = AES.new(key, AES.MODE_ECB)
     pt = ser.read(64)
     ct = ser.read(64)
     assert cipher.decrypt(pt) == ct, f"[!] Passed {i}, Failed {5-i}"
