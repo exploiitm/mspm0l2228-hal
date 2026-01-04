3ebecf10310ebb56c054bdc298d95a567b95ecc5
rishit khandelwal <rishitkh@gmail.com>
2026-01-04 19:42:00 +0530

    Fix CBC test script to use IV

--- examples/aes_cbc_test.py
+++ examples/aes_cbc_test.py
@@ -3,7 +3,10 @@ import time
 import subprocess
 from Crypto.Cipher import AES
 
-ser = serial.Serial('/dev/ttyACM0', timeout=10)
+ser = serial.Serial("/dev/ttyACM0", timeout=10)
+ser.reset_input_buffer()
+while ser.read(1):
+    pass
 subprocess.Popen(["bash", "reset.sh"], stderr=subprocess.DEVNULL)
 start_frame = bytes.fromhex("deadc0de")
 buffer = b"\x00"*4
@@ -12,36 +15,41 @@ print("[*] Recieving startframe");
 while True:
     buffer = (buffer + ser.read(1))[1:]
     if buffer == start_frame: break
-    # print("Debug:", buffer.hex())
+    print("Debug:", buffer.hex())
 print("[+] Recieved startframe");
 
 key = ser.read(16)
 print("[+] Recieved KEY")
 
 for i in range(5):
-    cipher = AES.new(key, AES.MODE_ECB)
+    iv = ser.read(16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
     pt = ser.read(16)
     ct = ser.read(16)
     assert cipher.encrypt(pt) == ct, f"[!] Passed {i}, Failed {5-i}"
-print("[+] Passed ECB Enc Tests [NoDma]")
+print("[+] Passed CBC Enc Tests [NoDma]")
 
 for i in range(5):
-    cipher = AES.new(key, AES.MODE_ECB)
+    iv = ser.read(16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
     pt = ser.read(16)
     ct = ser.read(16)
+
     assert cipher.decrypt(pt) == ct, f"[!] Passed {i}, Failed {5-i}"
-print("[+] Passed ECB Dec Tests [NoDma]")
+print("[+] Passed CBC Dec Tests [NoDma]")
 
 for i in range(5):
-    cipher = AES.new(key, AES.MODE_ECB)
+    iv = ser.read(16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
     pt = ser.read(64)
     ct = ser.read(64)
     assert cipher.encrypt(pt) == ct, f"[!] Passed {i}, Failed {5-i}"
-print("[+] Passed ECB Enc Tests [WithDma]")
+print("[+] Passed CBC Enc Tests [WithDma]")
 
 for i in range(5):
-    cipher = AES.new(key, AES.MODE_ECB)
+    iv = ser.read(16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
     pt = ser.read(64)
     ct = ser.read(64)
     assert cipher.decrypt(pt) == ct, f"[!] Passed {i}, Failed {5-i}"
-print("[+] Passed ECB Dec Tests [WithDma]")
+print("[+] Passed CBC Dec Tests [WithDma]")
